f7fccf1a4de360f56eb8461ebdb94a1f73270ac3
FabiolaObed <1538240@senati.pe>
2023-06-30 15:22:16 -0500

    practica2.0

--- practica2.py
+++ practica2.py
@@ -25,16 +25,51 @@ else:
 
 texto = "hola mundo python este es mi script "
 cont_a=0
+cont_e=0
+cont_i=0
+cont_o=0
+cont_u=0
+
 for i in texto:  
    if (i=="a" or i=="e"):
       if(i=="a"):
            cont_a += 1
            print("es una vocal")
-   elif(i==" "):
-     print("es un espacio")
+   else:
+      print("es una consonante")
+for i in texto:  
+   if (i=="e" or i=="e"):
+      if(i=="e"):
+           cont_e += 1
+           print("es una vocal")
+   else:
+      print("es una consonante")
+for i in texto:  
+   if (i=="i" or i=="e"):
+      if(i=="i"):
+           cont_i += 1
+           print("es una vocal")
+   else:
+      print("es una consonante")
+for i in texto:  
+   if (i=="o" or i=="e"):
+      if(i=="o"):
+           cont_o += 1
+           print("es una vocal")
+   else:
+      print("es una consonante")
+for i in texto:  
+   if (i=="u" or i=="e"):
+      if(i=="u"):
+           cont_u += 1
+           print("es una vocal")
    else:
       print("es una consonante")
 print(f"la cantidad de la vocal a es {cont_a }")
+print(f"la cantidad de la vocal e es {cont_e }")
+print(f"la cantidad de la vocal i es {cont_i }")
+print(f"la cantidad de la vocal o es {cont_o }")
+print(f"la cantidad de la vocal u es {cont_u }")
 
 
 
